f48ea8ed3c69b7428bb1338f15c58eedb3bb8131
Weiner Oliveira <woliveira82@gmail.com>
2023-02-10 20:09:51 +0000

    feat: enemy death and animation

--- Enemy.gd
+++ Enemy.gd
@@ -4,8 +4,18 @@ extends Node2D
 var hp = 25 setget set_hp
 
 onready var hp_label = $HpLabel
+onready var animation_player = $AnimationPlayer
 
+signal died
 
 func set_hp(new_hp):
 	hp = new_hp
 	hp_label.text = str(hp) + 'hp'
+	if hp <= 0:
+		emit_signal("died")
+		queue_free()
+
+	else:
+		animation_player.play("Shake")
+		yield(animation_player, "animation_finished")
+		animation_player.play("Attack")
