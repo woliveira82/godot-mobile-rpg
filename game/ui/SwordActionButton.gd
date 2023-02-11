4de00cee28186980db4bb60866b495a459d5d826
Weiner Oliveira <woliveira82@gmail.com>
2023-02-11 22:17:45 +0000

    feat: add slash effect

--- game/ui/SwordActionButton.gd
+++ game/ui/SwordActionButton.gd
@@ -1,11 +1,21 @@
 extends "res://game/ui/ActionButton.gd"
 
+const Slash = preload("res://game/Slash.tscn")
+
 
 func _on_pressed():
 	var main = get_tree().current_scene
 	var enemy = main.find_node("Enemy")
 	var player = main.find_node("PlayerStatus")
 	if enemy != null and player != null:
+		create_slash(enemy.global_position)
 		enemy.take_damage(4)
 		player.mp += 2
 		player.ap -= 1
+
+
+func create_slash(position):
+	var slash = Slash.instance()
+	var main = get_tree().current_scene
+	main.add_child(slash)
+	slash.global_position = position
